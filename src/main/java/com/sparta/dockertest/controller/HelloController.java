5ce096df8b52eb9ed47a51fcf419f8f503b95cd5
DOGWANNA <9401ndk@naver.com>
2023-02-27 15:26:23 +0900

    Initial commit

---
+++ src/main/java/com/sparta/dockertest/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.sparta.dockertest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String getHello() {return "Hello World";}
+}
